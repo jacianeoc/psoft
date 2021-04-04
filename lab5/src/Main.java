e30b9d0ba4151192446992c5be4301c460e85408
Jaciane Cruz <jaciane.cruz@ccc.ufcg.edu.br>
2021-04-04 20:26:30 -0300

    melhorando a interface textual

--- lab5/src/Main.java
+++ lab5/src/Main.java
@@ -10,21 +10,33 @@ public class Main {
         System.out.println("*********************");
         System.out.println("digite 1 para calcular rota");
         System.out.println("digite 2 para saber das rotas disponiveis");
+        System.out.println("digite 3 para sair da aplicação");
+        System.out.println("*********************");
 
-        opcao = sc.nextInt();
+        while (opcao!= 3){
+            System.out.print("digite uma opção: ");
+            opcao = sc.nextInt();
+            switch (opcao){
+                case 1:
+                    System.out.print("digite o tipo de transporte desejado: ");
+                    transporte = sc.next();
+                    System.out.println("\n"+ controllerTransporte.calculaRota(transporte) + "\n" );
+                    break;
 
-        switch (opcao){
-            case 1:
-                System.out.print("digite o tipo de transporte desejado: ");
-                transporte = sc.next();
-                System.out.println("\n" + controllerTransporte.calculaRota(transporte));
-                break;
+                case 2:
+                    System.out.println("lista de transportes disponiveis: ");
+                    System.out.println(controllerTransporte.transportesDisponiveis());
+                    break;
 
-            case 2:
-                System.out.println("lista de transportes disponiveis: ");
-                System.out.println(controllerTransporte.transportesDisponiveis());
-                break;
+                case 3:
+                    System.out.println("saindo da aplicação");
+                    break;
+
+                default:
+                    System.out.println("opcao invalida");
+            }
         }
+
 
 
     }
